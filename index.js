7cdadb9c88e7e80774ed3f96b06307789c1df9b7
Wen <vwen@china.pcs.local>
2017-08-17 18:15:02 +0800

    add ouput

--- index.js
+++ index.js
@@ -4,18 +4,43 @@ App.controller("AppController", [
   "$scope",
   function ($scope) {
 
-    $scope.data = [{
-      label: "Save",
-      suffix: undefined, 
-      model: null,
-    }, {
-      label: "Average interest rate",
-      suffix: "%", 
-      model: null,
-    }, {
-      label: "Duration",
-      suffix: "Years", 
-      model: null,
+    $scope.input = [{
+        label: "Current deposit",
+        suffix: undefined,
+        model: 0,
+      },
+      {
+        label: "Monthly save",
+        suffix: undefined,
+        model: 0,
+      }, {
+        label: "Average annual interest rate",
+        suffix: "%",
+        model: 0,
+      }, {
+        label: "Duration",
+        suffix: "Years",
+        model: 1,
+      }
+    ];
+
+    $scope.output = [{
+      label: "Final deposit",
+      model: null 
     }];
+
+    var deposit = $scope.input[0].model;
+
+    $scope.calculate = function () {
+      var deposit = $scope.input[0].model;
+      var save = $scope.input[1].model;
+      var rate = $scope.input[2].model;
+      var duration = $scope.input[3].model;
+
+      (deposit + (save * 12)) * rate
+
+      $scope.output[0].model = deposit;
+    };
+
   }
 ]);
